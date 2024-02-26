80a00c185c32bdadbc588ca3b9e3fb4e9f3c7fe9
Duygu Aras <109031940+DuyguAras@users.noreply.github.com>
2024-02-26 15:31:27 +0300

    Add: Highscore

--- Assets/Game/Scripts/Score/ScoreManager.cs
+++ Assets/Game/Scripts/Score/ScoreManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -10,16 +10,21 @@ namespace CoreGames.GameName
     {
 
         private float distance;
-        //private float highscore;
+        private float highscore;
 
         [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private TextMeshProUGUI highscoreText;
         [SerializeField] private GameObject startPosition;
         [SerializeField] private GameObject scoreTextObject;
+        [SerializeField] private GameObject highscoreTextObject;
 
-        //[SerializeField] private TextMeshProUGUI highscoreText;
+        
         void Start()
         {
             scoreText = scoreTextObject.GetComponent<TextMeshProUGUI>();
+            highscoreText = highscoreTextObject.GetComponent <TextMeshProUGUI>();
+            highscore = PlayerPrefs.GetInt("Highscore: ", 0);
+            
         }
         void Update()
         {
@@ -30,6 +35,18 @@ namespace CoreGames.GameName
         {
             distance = (startPosition.transform.position.z + this.transform.position.z);
             scoreText.text = distance.ToString("Score: " + "0");
+
+            AddHighscore((int)distance);
+        }
+
+        private void AddHighscore(int score)
+        {
+            if (score > highscore)
+            {
+                highscore = score;
+                highscoreText.text = "Highscore: " + highscore.ToString();
+                PlayerPrefs.GetInt("Highscore", 0);
+            }
         }
     }
 }
